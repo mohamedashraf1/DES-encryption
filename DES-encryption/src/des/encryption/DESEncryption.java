ae440fdb4e71ad9db8473f0992f270a179f984ea
mohamed_ashraf51 <36118944+mohamedashraf1@users.noreply.github.com>
2020-04-15 15:14:54 +0200

    the main code

---
+++ DES-encryption/src/des/encryption/DESEncryption.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package des.encryption;
+
+/**
+ *
+ * @author Elliot
+ */
+public class DESEncryption {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
