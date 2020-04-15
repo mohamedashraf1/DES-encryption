2d80d88e13f3569508ff6e17476046fb239056e3
mohamed_ashraf51 <36118944+mohamedashraf1@users.noreply.github.com>
2020-04-15 15:36:21 +0200

    comment

--- DES-encryption/src/des/encryption/DESEncryption.java
+++ DES-encryption/src/des/encryption/DESEncryption.java
@@ -5,15 +5,39 @@
  */
 package des.encryption;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Elliot
  */
 public class DESEncryption {
 
-    /**
-     * @param args the command line arguments
-     */
+    public String arrayToString(ArrayList<String> array){
+        StringBuffer sb = new StringBuffer();
+
+        for (String s : array) {
+           sb.append(s);
+        }
+        String str = sb.toString();
+        return str;
+    }
+    //mohamed ashraaf
+    public String XORing(String plainText, String key){
+        ArrayList<String> output = new ArrayList<String>();
+        String[] plainChars = plainText.split("");
+        String[] keyChars = key.split("");
+        for(int i = 0 ; i < key.length() ; i++){
+            if((plainChars[i].equals("1") && keyChars[i].equals("0"))  || (plainChars[i].equals("0") && keyChars[i].equals("1")))
+                output.add("1");
+            
+            else
+                output.add("0");
+        }
+        String tmp = arrayToString(output);
+        
+        return tmp;
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
