f7b6841ac9a5e1f7ed7bfe77a85a8de351cc5404
mohamed_ashraf51 <36118944+mohamedashraf1@users.noreply.github.com>
2020-04-16 02:22:56 +0200

    Decryption and fixed encryption

--- DES-encryption/src/des/encryption/DESEncryption.java
+++ DES-encryption/src/des/encryption/DESEncryption.java
@@ -8,6 +8,7 @@ package des.encryption;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -57,7 +58,49 @@ public class DESEncryption {
                             {7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8},
                             {2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11 }};
 
-
+    public int final_perm[] = { 40, 8, 48, 16, 56, 24, 64, 32, 
+                                39, 7, 47, 15, 55, 23, 63, 31, 
+                                38, 6, 46, 14, 54, 22, 62, 30, 
+                                37, 5, 45, 13, 53, 21, 61, 29, 
+                                36, 4, 44, 12, 52, 20, 60, 28, 
+                                35, 3, 43, 11, 51, 19, 59, 27, 
+                                34, 2, 42, 10, 50, 18, 58, 26, 
+                                33, 1, 41, 9, 49, 17, 57, 25 }; 
+    public int pc1[] = { 57, 49, 41, 33, 25, 
+                      17, 9, 1, 58, 50, 42, 34, 26, 
+                      18, 10, 2, 59, 51, 43, 35, 27, 
+                      19, 11, 3, 60, 52, 44, 36, 63, 
+                      55, 47, 39, 31, 23, 15, 7, 62, 
+                      54, 46, 38, 30, 22, 14, 6, 61, 
+                      53, 45, 37, 29, 21, 13, 5, 28, 
+                      20, 12, 4 }; 
+    public int pc2[] = {
+                14,17,11,24,1,5, 
+                3,28,15,6,21,10, 
+                23,19,12,4,26,8, 
+                16,7,27,20,13,2, 
+                41,52,31,37,47,55, 
+                30,40,51,45,33,48, 
+                44,49,39,56,34,53, 
+                46,42,50,36,29,32 
+              };
+    public int permutation_table[] = {
+            16,7,20,21,29,12,28,17, 
+            1,15,23,26,5,18,31,10, 
+            2,8,24,14,32,27,3,9, 
+            19,13,30,6,22,11,4,25 
+        };
+    
+    int initial_permutation_table[] ={58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7};
+    int expansion_table[] = {   
+                    32,1,2,3,4,5,4,5, 
+                    6,7,8,9,8,9,10,11, 
+                    12,13,12,13,14,15,16,17, 
+                    16,17,18,19,20,21,20,21, 
+                    22,23,24,25,24,25,26,27, 
+                    28,29,28,29,30,31,32,1 
+                  }; 
+    
     public ArrayList<String> readfile(String path) throws IOException {
         File file = new File(path);
         ArrayList<String> temp = new ArrayList<String>();
@@ -104,7 +147,7 @@ public class DESEncryption {
         ArrayList<String> data = readfile(path);
         String plain = data.get(0);
         String binary = "";
-        //System.out.println("im in");
+        
         for(int i = 0 ; i < plain.length() ; i++){
             int num = (int)plain.charAt(i);//get ascii
             String tmp = Integer.toBinaryString(num); //get binary  0 1110101
@@ -113,7 +156,7 @@ public class DESEncryption {
             }
             binary += tmp;
         }
-        //System.out.println(binary);
+        
         for(int i = 0 ; i < binary.length() ; i+=64){
             String tmp= "";
             for(int j = i ; j < i+64 ; j++){
@@ -123,7 +166,7 @@ public class DESEncryption {
             }
             chars.add(tmp);
         }
-        //System.out.println("dklfa");
+        
         if(chars.get(chars.size()-1).length() != 64){
             String tmp2 = chars.get(chars.size()-1);
             while(tmp2.length() < 64){//add the missing bits
@@ -134,31 +177,56 @@ public class DESEncryption {
         return chars;
     }
     public String readKey() throws IOException{
-        String path = "F:\\college\\third year\\second term\\Computer Network Security\\assignments\\DES-encryption\\DES-encryption\\key.txt";
-        ArrayList<String> chars = make64(path);
+        //String path = "F:\\college\\third year\\second term\\Computer Network Security\\assignments\\DES-encryption\\DES-encryption\\key.txt";
+        //ArrayList<String> chars = make64(path);
         
-        String key = chars.get(0);
-        while(key.length() != 8)
+        String key = "";
+        
+        while(key.length() != 64)
         {
+            key = "";
             System.out.println("this  key is too short please enter 8 characters ");
             Scanner scanner = new Scanner(System.in);
-            key = scanner.nextLine();
+            String tmpkey = scanner.nextLine();
+            
+            for(int i = 0 ;i < tmpkey.length() ; i++){
+                int num = (int)tmpkey.charAt(i);//get ascii
+                String tmp = Integer.toBinaryString(num); //get binary  0 1110101
+                while(tmp.length() < 8){//add the missing bits
+                    tmp = "0" + tmp;
+                }
+                key += tmp;
+            }
         }
-        int pc1[] = { 
-            57,49,41,33,25,17,9, 
-            1,58,50,42,34,26,18, 
-            10,2,59,51,43,35,27, 
-            19,11,3,60,52,44,36,           
-            63,55,47,39,31,23,15, 
-            7,62,54,46,38,30,22, 
-            14,6,61,53,45,37,29, 
-            21,13,5,28,20,12,4 
-          };
+        
+        
         String keypc1 = "";
-        for(int i = 0 ; i < pc1.length ; i++){
+        for(int i = 0 ; i < 56 ; i++){
             keypc1 += key.charAt(pc1[i] - 1);
         }
+        
         return keypc1;
+    }
+    public String rightShift(String data, int num){
+        String shift = "";
+        if  (num == 1){
+            shift += data.charAt(27);
+            for (int  i = 0 ; i < 27 ; i++)
+            {
+                 shift += data.charAt(i);
+            }
+        
+        }
+        else{
+            shift += data.charAt(26);
+            shift += data.charAt(27);
+            for (int  i = 0 ; i < 26 ; i++)
+             {
+                 shift += data.charAt(i);
+             }
+        }
+                
+        return shift; 
     }
     public String leftShift(String data, int num)
     {
@@ -177,7 +245,8 @@ public class DESEncryption {
              {
                  shift += data.charAt(i);
              }
-            shift += data.charAt(0) + data.charAt(1);
+            shift += data.charAt(0);
+            shift += data.charAt(1);
         }
                 
         return shift; 
@@ -186,7 +255,9 @@ public class DESEncryption {
         ArrayList<String> keys = new ArrayList<String>();
         String key = readKey();
         String left = "" , right ="";
+        
         for(int i = 1 ; i< 17 ; i++){
+            
             for(int j = 0 ; j < 56 ; j++){
                 if(j < 28){
                     left += key.charAt(j);
@@ -204,17 +275,52 @@ public class DESEncryption {
             }
             key = left + right;
             
-            int pc2[] = {
-                14,17,11,24,1,5, 
-                3,28,15,6,21,10, 
-                23,19,12,4,26,8, 
-                16,7,27,20,13,2, 
-                41,52,31,37,47,55, 
-                30,40,51,45,33,48, 
-                44,49,39,56,34,53, 
-                46,42,50,36,29,32 
-              };
+            
             String tmp="";
+            for(int j = 0 ; j < pc2.length ;j++){
+                tmp += key.charAt(pc2[j] - 1);
+                
+            }
+            left ="";right ="";
+            
+            keys.add(tmp);
+            
+        }
+        
+        return keys;
+    }
+    public ArrayList<String> generateKeyDec() throws IOException{
+        ArrayList<String> keys = new ArrayList<String>();
+        String key = readKey();
+        String left = "" , right ="";
+        
+        String tmp="";
+        for(int j = 0 ; j < pc2.length ;j++){
+            tmp += key.charAt(pc2[j] - 1);
+        }
+        keys.add(tmp);
+        
+        for(int i = 2 ; i< 17 ; i++){
+            
+            for(int j = 0 ; j < 56 ; j++){
+                if(j < 28){
+                    left += key.charAt(j);
+                }
+                else
+                    right += key.charAt(j);
+            }
+            if(i == 1 || i == 2 || i == 9 || i == 16){
+                right = rightShift(right,1);
+                left = rightShift(left,1);
+            }
+            else{
+                right = rightShift(right,2);
+                left = rightShift(left,2);
+            }
+            key = left + right;
+            left = "";right = "";
+            
+            tmp="";
             for(int j = 0 ; j < pc2.length ;j++){
                 tmp += key.charAt(pc2[j] - 1);
             }
@@ -242,8 +348,12 @@ public class DESEncryption {
         return dec_value;
     }
     public String S_boxing(String tmp,int round){
-        String row = tmp.charAt(5) + tmp.charAt(0) +"";
-        String col = tmp.charAt(1) + tmp.charAt(2) + tmp.charAt(3) + tmp.charAt(4) +"";
+        String row = tmp.charAt(5) + "";
+        row += tmp.charAt(0);
+        String col = tmp.charAt(1)+"" ;
+        col += tmp.charAt(2);
+        col += tmp.charAt(3);
+        col += tmp.charAt(4);
         int indxrow = toDesimal(row);
         int indxcol = toDesimal(col);
         int value ;
@@ -295,28 +405,24 @@ public class DESEncryption {
             String tmp = S_boxing(chars.get(i-1),i);
             func += tmp;
         }
-        int permutation_table[] = {
-            16,7,20,21,29,12,28,17, 
-            1,15,23,26,5,18,31,10, 
-            2,8,24,14,32,27,3,9, 
-            19,13,30,6,22,11,4,25 
-        };
+        
         String perm = "";
         for(int i = 0 ; i< 32 ; i++){
             perm += func.charAt(permutation_table[i] -1);
         }
         return perm;
     }
+    
     public String encrypt() throws IOException{
         String path = "F:\\college\\third year\\second term\\Computer Network Security\\assignments\\DES-encryption\\DES-encryption\\plain.txt";
         ArrayList<String> chars = make64(path);
         ArrayList<String> keys = generateKey();
         String cipher = "";
         for(int i =0 ; i < chars.size() ; i++){
-            //String Key = readKey();
+            
             String tmp = chars.get(i);
             
-            int initial_permutation_table[] ={58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7}; 
+             
             String perm ="";
             for(int j = 0 ; j < 64 ; j++){
                 perm += tmp.charAt(initial_permutation_table[j] -1);
@@ -332,14 +438,99 @@ public class DESEncryption {
                         right += perm.charAt(j);
                 }
                 String expand = "";
-                int expansion_table[] = {   
-                    32,1,2,3,4,5,4,5, 
-                    6,7,8,9,8,9,10,11, 
-                    12,13,12,13,14,15,16,17, 
-                    16,17,18,19,20,21,20,21, 
-                    22,23,24,25,24,25,26,27, 
-                    28,29,28,29,30,31,32,1 
-                  }; 
+                
+                
+                for(int j = 0 ; j < expansion_table.length ; j++){
+                    expand += right.charAt(expansion_table[j] - 1);
+                }
+                
+                String fFun = F_Function(expand, keys.get(r));
+                left = XORing(left, fFun);
+                
+                String swap = left;
+                left = right;
+                right = swap;
+                
+                perm = left + right;
+                
+                
+            }
+            String swap2 = left;
+            left = right;
+            right = swap2;
+            
+            perm = left + right;
+            
+            
+            String finalperm = "";
+            for(int j = 0 ; j < final_perm.length ; j++){
+                finalperm += perm.charAt(final_perm[j] - 1);     
+            }
+            cipher += finalperm;
+            
+        }
+        String file = "";
+        for(int i = 0 ; i < cipher.length() ;i+=8){
+            int ascci  = Integer.parseInt(cipher.substring(i, i+8), 2);
+            file +=   Integer.toString(ascci) + " ";
+        }
+        FileWriter fw=new FileWriter("F:\\college\\third year\\second term\\Computer Network Security\\assignments\\DES-encryption\\DES-encryption\\cipherd.txt");
+        for (int i = 0; i < file.length(); i++) 
+            fw.write(file.charAt(i));
+        fw.close();
+        
+        return cipher;
+    }
+    public String decrypt() throws IOException{
+        String path = "F:\\college\\third year\\second term\\Computer Network Security\\assignments\\DES-encryption\\DES-encryption\\cipherd.txt";
+        ArrayList<String> ascii = readfile(path);
+        ArrayList<String> keys = generateKeyDec();
+        ArrayList<String> chars = new ArrayList<String>();
+        String code = arrayToString(ascii);
+        String nums[] = code.split(" ");
+        String ciphered = "";
+        for(int i = 0 ; i < nums.length ; i++){
+            int num = Integer.parseInt(nums[i]);
+            String tmp = Integer.toBinaryString(num); //get binary  0 1110101
+            while(tmp.length() < 8){//add the missing bits
+                tmp = "0" + tmp;
+            }
+            ciphered += tmp;
+        }
+        for(int i = 0 ; i < ciphered.length() ; i+=64){
+            String tmp= "";
+            for(int j = i ; j < i+64 ; j++){
+                if(j == ciphered.length())
+                    break;
+                tmp += ciphered.charAt(j);
+            }
+            chars.add(tmp);
+        }
+        
+        
+        //-----------------------------------------
+        String decipher = "";
+        for(int i =0 ; i < chars.size() ; i++){
+            //String Key = readKey();
+            String tmp = chars.get(i);
+            
+             
+            String perm ="";
+            for(int j = 0 ; j < 64 ; j++){
+                perm += tmp.charAt(initial_permutation_table[j] -1);
+            }
+            String left = "" , right ="";
+            for(int r = 0 ; r < 16 ; r++){
+                left = "" ; right = "";
+                for(int j = 0 ; j < 64 ; j++){
+                    if(j < 32){
+                        left += perm.charAt(j);
+                    }
+                    else
+                        right += perm.charAt(j);
+                }
+                String expand = "";
+                
                 
                 for(int j = 0 ; j < expansion_table.length ; j++){
                     expand += right.charAt(expansion_table[j] - 1);
@@ -362,27 +553,27 @@ public class DESEncryption {
             
             perm = left + right;
             
-            int final_perm[] = { 40, 8, 48, 16, 56, 24, 64, 32, 
-                                39, 7, 47, 15, 55, 23, 63, 31, 
-                                38, 6, 46, 14, 54, 22, 62, 30, 
-                                37, 5, 45, 13, 53, 21, 61, 29, 
-                                36, 4, 44, 12, 52, 20, 60, 28, 
-                                35, 3, 43, 11, 51, 19, 59, 27, 
-                                34, 2, 42, 10, 50, 18, 58, 26, 
-                                33, 1, 41, 9, 49, 17, 57, 25 }; 
+            
             String finalperm = "";
             for(int j = 0 ; j < final_perm.length ; j++){
                 finalperm += perm.charAt(final_perm[j] - 1);     
             }
-            cipher += finalperm;
+            decipher += finalperm;
+        
         }
-        System.out.println(cipher);
-        return cipher;
+        String str = "";
+        for (int i = 0; i < decipher.length()/8; i++) {
+            int a = Integer.parseInt(decipher.substring(8*i,(i+1)*8),2);
+            str += (char)(a);
+        }
+
+        System.out.println(str);
+        return decipher;
     }
-    
     public static void main(String[] args) throws IOException {
         DESEncryption des = new DESEncryption();
         des.encrypt();
+        des.decrypt();
     }
     
 }
